cfb9b851d0d2f808ebb65f6e45dd6619c5d88a38
Acriid <wilrichgerdus@gmail.com>
2026-06-02 00:05:02 +0200

    Added fonts

--- JS/projectdata.js
+++ JS/projectdata.js
@@ -5,6 +5,7 @@ export const projectLinks = [
   {
     name: "Project 1", 
     description: "Description 1",
+    longDescriptionRef: "/Text/Projects/project1.txt",
     href: "/Pages/Projects/project1.html", 
     imgref: "/Assets/Images/Projects/Temp/FairyFlight.jpg", 
     complete: false,
@@ -13,6 +14,7 @@ export const projectLinks = [
   {
     name: "Project 2", 
     description: "Description 2",
+    longDescriptionRef: "/Text/Projects/project2.txt",
     href: "/Pages/Projects/project2.html", 
     imgref: "/Assets/Images/Projects/Temp/fairy.jpg", 
     complete: false,
@@ -21,6 +23,7 @@ export const projectLinks = [
   {
     name: "Project 3", 
     description: "Description 3",
+    longDescriptionRef: "/Text/Projects/project3.txt",
     href: "/Pages/Projects/project3.html", 
     imgref: "/Assets/Images/Projects/Temp/FairyMagic.jpg", 
     complete: false,
@@ -29,6 +32,7 @@ export const projectLinks = [
   {
     name: "Project 4", 
     description: "Description 4",
+    longDescriptionRef: "/Text/Projects/project4.txt",
     href: "/Pages/Projects/project4.html", 
     imgref: "/Assets/Images/Projects/Temp/VoidDevastator.png", 
     complete: false,
